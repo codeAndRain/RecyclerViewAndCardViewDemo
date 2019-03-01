cdcd379b42f688c3374a285257e658440d28f274
Desmond <user.example.com>
2019-03-01 11:43:01 +0000

    start activity by passing Employee object using Serializable

---
+++ app/src/main/java/com/training/recyclerviewandcardview/view/DetailsActivity.java
@@ -0,0 +1,31 @@
+package com.training.recyclerviewandcardview.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.training.recyclerviewandcardview.R;
+import com.training.recyclerviewandcardview.model.Employee;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    public static final String EMPLOYEE_EXTRA = "EMPLOYEE_EXTRA";
+    TextView employeeNameText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        employeeNameText = findViewById(R.id.employee_name_textview);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null) {
+            Employee employee = (Employee) bundle.getSerializable(EMPLOYEE_EXTRA);
+            if (employee != null) {
+                employeeNameText.setText(employee.getName());
+            }
+        }
+    }
+}
